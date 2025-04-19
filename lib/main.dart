d2c2a4d02ce715046503a6401316ac116d561188
Gabriel Felix <102039571+gabrieelfelix@users.noreply.github.com>
2025-04-19 16:23:55 -0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:attendance_repository/attendance_repository.dart';
 
 import 'package:club_repository/club_repository.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -18,7 +20,13 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await setupDependences();
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
+  //runApp(MyApp());
 }
 
 final getIt = GetIt.instance;
